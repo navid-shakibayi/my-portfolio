e88d92f756e8a977d4e49a2a03902e0dcbf7f8cd
navid-shakibayi <naweed.sh1474@gmail.com>
2024-05-04 16:48:43 +0330

    ProjectPage changes

---
+++ src/Components/ProjectsPage/Project.jsx
@@ -0,0 +1,10 @@
+const Project = ({
+    projectName,
+}) => {
+
+    return <>
+        <h1>{projectName}</h1>
+    </>
+}
+
+export default Project
--- src/Components/ProjectsPage/ProjectsPage.jsx
+++ src/Components/ProjectsPage/ProjectsPage.jsx
@@ -1,11 +1,60 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import Loading from "../Shared/Loading"
+import SectionTitle from "../Shared/SectionTitle"
+import Project from "./Project"
+
+
 const ProjectsPage = ({
 
 }) => {
 
+    const [projectsPageData, setProjectsPageData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        fetch('https://my-portfolio.chbk.run/api/projects-page')
+            .then(res => res.json())
+            .then(data => {
+                setProjectsPageData(data.data)
+                setLoading(false)
+                setError(null)
+            }).catch(err => {
+                setLoading(false)
+                setError(err.message)
+            })
+    }, [])
+
+    const [projectData, setProjectData] = useState(null)
+
+    useEffect(() => {
+        fetch('https://my-portfolio.chbk.run/api/projects?populate=*')
+            .then(res => res.json())
+            .then(data => {
+                setProjectData(data.data)
+                setLoading(false)
+                setError(null)
+            }).catch(err => {
+                setLoading(false)
+                setError(err.message)
+            })
+    }, [])
+
     return <>
-    <section className="max-w-6xl mx-auto px-6">
-        teeeeeeeeeeest
-    </section>
+        <section className="max-w-6xl mx-auto px-6">
+            {loading && <><Loading /></>}
+
+            {projectsPageData && <>
+                <SectionTitle content={projectsPageData.attributes.sectionTitle} />
+            </>}
+
+            {projectData && projectData.map(item => {
+                return <section>
+                    <h1>{item.attributes.projectName}</h1>
+                </section>
+            })}
+        </section>
     </>
 }
 
